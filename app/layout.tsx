3c529102370279a6696b178c38e5a8680008eea0
Jang Eunhye <hh961130@naver.com>
2025-02-23 16:25:28 +0900

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import { Metadata } from "next";
+import Navigation from "../components/navigation";
+
+export const metadata :Metadata = {
+  title: {
+    template: "%s | Next Movies",
+    default: "Next Movies",
+  },
+}
+
+export default function Layout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <Navigation />
+        {children}
+      </body>
+    </html>
+  );
+}
