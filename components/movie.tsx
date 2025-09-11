bd856e69c9bf398f05c0b2e18b5c93df5d1321c1
Jang Eunhye <hh961130@naver.com>
2025-09-11 17:35:27 +0900

    영화 상세페이지 기본 탭 선택(credits)

--- components/movie.tsx
+++ components/movie.tsx
@@ -17,7 +17,7 @@ export default function Movie({ title, id, poster_path }: IMovieProps) {
   return (
     <div className={styles.movie}>
       <img src={poster_path} alt={title} onClick={onClick} />
-      <Link prefetch href={`/movies/${id}`}>
+      <Link prefetch href={`/movies/${id}?tab=credits`}>
         {title}
       </Link>
     </div>
